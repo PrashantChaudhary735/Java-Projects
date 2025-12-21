0636d1a8939b58662f531df16e99f5aa97bbbd95
Prashant Chaudhary <chaudharyprashant7355@gmail.com>
2025-12-21 20:18:30 +0530

    String Methods in java

---
+++ src/StringMethods.java
@@ -0,0 +1,16 @@
+public class StringMethods {
+    static void main(String[] args) {
+        String a = "ALEXX";
+        String b = new String("JOHN");
+        String c = new String("Testing purpose");
+        System.out.println(a.length());
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
+        System.out.println(a.trim());
+        System.out.println(a.substring(0,3));
+        System.out.println(a.replace('X', 'Y'));
+        System.out.println(a.charAt(0));
+
+
+    }
+}
